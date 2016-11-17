b69475fc2e876872af2ae7e8ed54477749494c8c
JensGe <jens@honzont.de>
2016-11-17 17:31:17 +0100

    BUILD with JUnit = False

---
+++ src/test/java/de/honzont/MainTest.java
@@ -0,0 +1,18 @@
+package test.java.de.honzont;
+
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Created by Gäbeler on 17.11.2016.
+ */
+public class MainTest {
+
+    @Test
+    public void testOne()
+    {
+        assertTrue( "TestExample", true );
+    }
+}
